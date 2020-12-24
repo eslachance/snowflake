14ee52fe6ea252bd23249423a92e544021bffe6d
Vaalha <code@vaalha.com>
2020-12-23 22:33:41 -0500

    Fix tailwind production build

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'tailwindcss/dist/tailwind.css';
+import './style.css';
 
 import App from './App.jsx';
 
